4bd4ee0414d9f8bb9a5afe0e1e7ad7cca65ecd7d
maxim <maxim.pavlenko2011@gmail.com>
2025-04-12 22:56:00 +0200

    GDT fixed

--- kernel/src/arch/x86-64/gdt.c
+++ kernel/src/arch/x86-64/gdt.c
@@ -7,9 +7,9 @@ struct gdt_descriptor gdt_desc;
 void init_gdt() {
         int idx = 0;
         gdt_table[idx++] = 0x0000000000000000;  // Null descriptor
-        gdt_table[idx++] = 0x00CF9A000000FFFF;  // Kernel code segment
+        gdt_table[idx++] = 0x00AF9A000000FFFF;  // Kernel code segment
         gdt_table[idx++] = 0x00CF92000000FFFF;  // Kernel data segment
-        gdt_table[idx++] = 0x00CFFA000000FFFF;  // User code segment
+        gdt_table[idx++] = 0x00AFFA000000FFFF;  // User code segment
         gdt_table[idx++] = 0x00CFF2000000FFFF;  // User data segment
 
         gdt_desc.size = sizeof(gdt_table) - 1;
